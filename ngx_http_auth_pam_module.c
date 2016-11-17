02d1f716e3c77343f4787387d079baa6a0f4cab2
Luo ZengBin <jalen.cn@gmail.com>
2016-11-17 22:35:59 +0900

    test...

--- ngx_http_auth_pam_module.c
+++ ngx_http_auth_pam_module.c
@@ -208,9 +208,11 @@ ngx_http_auth_pam_handler(ngx_http_request_t *r)
     ngx_http_auth_pam_ctx_t  *ctx;
     ngx_http_auth_pam_loc_conf_t  *alcf;
 
-    /* ngx_http_variable_value_t *vflag; */
-    /* ngx_str_t  key = ngx_string("skip_pam_auth"); */
+    ngx_http_variable_value_t *vflag;
+    ngx_str_t  key = ngx_string("skip_pam_auth");
     /* vflag = ngx_http_get_variable(r, &key, ngx_hash_key(key.data, key.len)); */
+
+    return NGX_OK;
 
     /* ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "skip_pam_auth:%s", vflag->data); */
     /* if(ngx_strcmp(vflag->data, "true") == 0) { */
